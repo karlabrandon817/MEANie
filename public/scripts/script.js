09d777caf69681190c662525d81453aaf0c1489a
Karla Hugo <karlabrandon817@gmail.com>
2017-01-09 13:58:28 -0600

    more readable formatting

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,29 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-$scope.addRecord = function(){
-event.preventDefault();
-var objectToSend ={
-name: $scope.nameIn,
-location: $scope.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-$scope.nameIn ='';
-$scope.locationIn='';
-};
-$scope.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-$scope.allTheRecords = response;
-console.log( $scope.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+var myApp = angular.module('myApp', []);
+myApp.controller('whereMyPeeps', ['$scope', '$http', function($scope, $http) {
+    $scope.addRecord = function() {
+        event.preventDefault();
+        var objectToSend = {
+            name: $scope.nameIn,
+            location: $scope.locationIn,
+        };
+        $http({
+            method: 'POST',
+            url: '/testPost',
+            data: objectToSend
+        });
+        $scope.nameIn = '';
+        $scope.locationIn = '';
+    };
+    $scope.getRecords = function() {
+        $.http({
+                method: 'GET',
+                url: '/getRecords',
+            }).then(function(response) {
+                $scope.allTheRecords = response;
+                console.log($scope.allTheRecords);
+            }),
+            function myError(response) {
+                console.log(response.statusText);
+            };
+    };
 }]);
--- server/app.js
+++ server/app.js
@@ -1,39 +1,48 @@
-var express = require( 'express' );
-var app=express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-app.use( bodyParser.json() );
-var mongoose = require( 'mongoose' );
-// 27017 is default mongo port
-mongoose.connect( 'localhost:/27017/test' );
-var ourSchema = new  mongoose.Schema({
-name: String
-location: String
-});
-var ourModel = mongoose.model( 'ourModel', ourSchema );
-app.get( '/', function( req, res ){
-res.sendFile( path.resolve( 'public/index.html' ) );
-});
-app.get( '/getRecords', function( req, res ){
-// get and send back all the things
-ourModel.find();
-.then( function( data ){
-res.send( data );
-});
-});
-app.listen( 8080, 'localhost', function( req, res ){
-console.log( 'listening on 8080' );
-});
-app.post( '/testPost', function( req, res ){
-console.log( 'req.body.name: ' + req.body.name );
-// retrieved the req.body
-// putting it into an object to be saved in the db
-var recordToAdd={
-name=req.body.name,
-location=req.body.location
-}
-// create new record
-var newRecord=ourModel( recordToAdd );
-newRecord.save();
-});
-app.use( express.static( 'public' ) );
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+mongoose.connect('localhost:/27017/test');
+
+
+var ourSchema = mongoose.Schema({
+    name: String,
+    location: String
+}); //end ourSchema
+
+var ourModel = mongoose.model('ourModel', ourSchema);
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// -- spin up server -- //
+app.listen(8080, 'localhost', function(req, res) {
+    console.log('listening on 8080');
+}); //end spin up server
+
+// -- base url -- //
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('public/index.html'));
+}); //end base url
+
+app.get('/getRecords', function(req, res) {
+    // get and send back all the things
+    ourModel.find().then(function(data) {
+        res.send(data);
+    }); //end find
+}); //end getRecords
+
+app.post('/testPost', function(req, res) {
+    console.log('req.body.name: ' + req.body.name);
+    // retrieved the req.body
+    // putting it into an object to be saved in the db
+    var recordToAdd = {
+        name: req.body.name,
+        location: req.body.location
+    };
+    // create new record
+    var newRecord = ourModel(recordToAdd);
+    newRecord.save();
+}); // end testPost
